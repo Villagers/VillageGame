97a066bf8a1e7cae57a7353063376a9a3d02b7f3
Arthur Xu <r2ysxu@gmail.com>
2015-06-06 01:16:01 -0400

    Removed old images and added heightmap

--- scripts/game.js
+++ scripts/game.js
@@ -12,7 +12,11 @@ function initialize() {
 
     document.getElementById('gameDiv').appendChild(renderer.domElement);
 
-    scene.add(generateGround());
+    var heightmap = new Image();
+    heightmap.src = "resources/images/heightmap.png";
+    heightmap.onload = function () {
+        scene.add(generateGround(heightmap));
+    }
 
     camera.position.y = -50;
     camera.rotation.x = 150 * (Math.PI/180);
@@ -41,16 +45,68 @@ function initialize() {
     animate();
 }
 
-function generateGround() {
-    var geometry = new THREE.PlaneBufferGeometry(1000, 1000);
-    var texture = THREE.ImageUtils.loadTexture("resources/images/grass5.jpg");
-    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set(10, 10);
-    var material = new THREE.MeshBasicMaterial( { map: texture, side: THREE.DoubleSide } );
-    var mesh = new THREE.Mesh(geometry, material);
+//return array with height data from img
+function getHeightData(heightmapImage,scale) {
+  
+ if (scale == undefined) scale=1;
+  
+    var canvas = document.createElement( 'canvas' );
+    canvas.width = heightmapImage.width;
+    canvas.height = heightmapImage.height;
+    var context = canvas.getContext( '2d' );
+ 
+    var size = heightmapImage.width * heightmapImage.height;
+    var data = new Float32Array( size );
+ 
+    context.drawImage(heightmapImage,0,0);
+ 
+    for ( var i = 0; i < size; i ++ ) {
+        data[i] = 0
+    }
+ 
+    var imgd = context.getImageData(0, 0, heightmapImage.width, heightmapImage.height);
+    var pix = imgd.data;
+ 
+    var j=0;
+    for (var i = 0; i<pix.length; i +=4) {
+        var all = pix[i]+pix[i+1]+pix[i+2];
+        data[j++] = all/(12*scale);
+    }
+     
+    return data;
+}
 
-    mesh.rotation.x = Math.PI/2;
-    return mesh;
+function generateLight() {
+    var light = new THREE.DirectionalLight(0xffffff);
+    // light.position.set( 0, 0, 0 ).normalize();
+    return light;
+}
+
+
+function generateGround(heightmapImage) {
+    var groundPlane = new THREE.PlaneBufferGeometry(1000, 1000, 597, 597);
+    groundPlane.dynamic = true;
+    // Set Ground height
+    var data = getHeightData(heightmapImage);
+    var vertices = groundPlane.attributes.position.array;
+    console.log(vertices.length);
+    console.log(data.length);
+    for (var i = 0; i < data.length; i++) {
+         vertices[2 + i*3] = data[i];
+    }
+    groundPlane.attributes.position.needsUpdate = true;
+
+    // Set Texture
+    var groundTexture = THREE.ImageUtils.loadTexture("resources/images/grass5.jpg");
+    groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+    groundTexture.repeat.set(10, 10);
+
+    var material = new THREE.MeshBasicMaterial( { map: groundTexture, side: THREE.DoubleSide } );
+    var groundMesh = new THREE.Mesh(groundPlane, material);
+
+    // Rotations
+    groundMesh.rotation.x = Math.PI/2;
+    return groundMesh;
 }
 
 function animate(){
